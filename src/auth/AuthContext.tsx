00ea14d27c6fac113bc24b44fe5634a8b2b908fa
Priyansh Sahu <priyansh.sahu.cer21@itbhu.ac.in>
2025-06-20 22:15:29 +0530

    fixed duplicate api calls

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type ReactNode } from 'react';
+import { useEffect, useState, useRef, type ReactNode } from 'react';
 import { AuthContext } from './AuthContextValue';
 import type { AuthResponse } from '../api/types';
 import { refreshToken } from '../api/auth';
@@ -6,39 +6,44 @@ import { useNavigate } from 'react-router-dom';
 import LoadingPage from '../components/loaderPage';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<AuthResponse| null>(null);
+  const [user, setUser] = useState<AuthResponse | null>(null);
   const [loading, setLoading] = useState(true);
+  const hasRunRef = useRef(false);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    const checkUserSession = async()=>{
+  useEffect(() => {
+    if (hasRunRef.current) return;
+    hasRunRef.current = true;
+
+    const checkUserSession = async () => {
       try {
         const response = await refreshToken();
         login(response.data);
-        navigate('/'); 
+        navigate('/');
       } catch {
         logout();
-      }finally {
+      } finally {
         setLoading(false);
       }
-    }
+    };
+
     checkUserSession();
-  });
+  }, []);
 
   const login = (userData: AuthResponse) => setUser(userData);
   const logout = () => setUser(null);
 
   const authContextValue = {
-      user,
-      login,
-      logout,
-      isAuthenticated: !!user,
-      loading, 
-    };
+    user,
+    login,
+    logout,
+    isAuthenticated: !!user,
+    loading,
+  };
 
   return (
     <AuthContext.Provider value={authContextValue}>
-      {loading ? <LoadingPage/> :children}
+      {loading ? <LoadingPage /> : children}
     </AuthContext.Provider>
   );
 };
